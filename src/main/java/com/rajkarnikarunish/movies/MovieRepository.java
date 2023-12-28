c60833df975bd22255028473cd83a36106142fa8
Unish Rajkarnikar <ubidy947@gmail.com>
2023-12-27 19:57:10 -0600

    feat: create review, added reviews service, controller repository

--- src/main/java/com/rajkarnikarunish/movies/MovieRepository.java
+++ src/main/java/com/rajkarnikarunish/movies/MovieRepository.java
@@ -4,11 +4,15 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-}
+import java.util.Optional;
 
 /***
  * This class is a data access layer
  * It does the job of talking to database
  */
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
+
--- src/main/java/com/rajkarnikarunish/movies/MovieService.java
+++ src/main/java/com/rajkarnikarunish/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.rajkarnikarunish.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -17,5 +19,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singelMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
