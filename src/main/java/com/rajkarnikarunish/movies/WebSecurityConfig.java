d84d5691e91cec55c2b646f7ec759d367fd7b6bd
Unish Rajkarnikar <ubidy947@gmail.com>
2023-12-28 21:17:22 -0600

    websecurityconfig added

---
+++ src/main/java/com/rajkarnikarunish/movies/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.rajkarnikarunish.movies;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends SecurityConfigurerAdapter {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable();
+        return http.build();
+    }
+}
